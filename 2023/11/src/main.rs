d20fe960713d3b5fa9455964aa8bba556dc9484a
asciiboi <56585404+asciiboi@users.noreply.github.com>
2023-12-12 20:08:32 +0100

    Day 11 Part 2

--- 2023/11/src/main.rs
+++ 2023/11/src/main.rs
@@ -1,14 +1,14 @@
-const DOMAIN_EXPANTION_VALUE: i32 = 2;
+const DOMAIN_EXPANTION_VALUE: i64 = 1_000_000;
 
 struct Galaxy {
-    x: i32,
-    y: i32
+    x: i64,
+    y: i64
 }
 
 
-fn galaxy_manhattan_distance(g1: &Galaxy, g2: &Galaxy) -> i32 {
-    let x_dist: i32 = (g2.x - g1.x).abs();
-    let y_dist: i32 = (g2.y - g1.y).abs();
+fn galaxy_manhattan_distance(g1: &Galaxy, g2: &Galaxy) -> i64 {
+    let x_dist: i64 = (g2.x - g1.x).abs();
+    let y_dist: i64 = (g2.y - g1.y).abs();
     x_dist + y_dist
 }
 
@@ -16,15 +16,15 @@ fn main() {
     // Collect the input as a list of bools. True is galaxy, false is not.
     let input: Vec<Vec<bool>> = include_str!("../input.txt").split("\n").filter(|x| !x.is_empty()).map(|x| x.chars().map(|x| x=='#').collect()).collect();
 
-    let mut vertical_expantions: Vec<i32> = vec![];
+    let mut vertical_expantions: Vec<i64> = vec![];
 
     for (y, line) in input.iter().enumerate() {
         if !line.contains(&true) {
-            vertical_expantions.push(y.clone() as i32);
+            vertical_expantions.push(y.clone() as i64);
         }
     }
 
-    let mut horizontal_expantions: Vec<i32> = vec![];
+    let mut horizontal_expantions: Vec<i64> = vec![];
     let width = input.first().unwrap().len();
     for x in 0..width {
         let mut contains_galaxy = false;
@@ -34,7 +34,7 @@ fn main() {
             }
         }
         if !contains_galaxy {
-            horizontal_expantions.push(x.clone() as i32);
+            horizontal_expantions.push(x.clone() as i64);
         }
     }
     
@@ -44,18 +44,18 @@ fn main() {
 
     for (y, line) in input.iter().enumerate() {
         for (x, v) in line.iter().enumerate() {
-            let v_domains = vertical_expantions.iter().filter(|ry| ry < &&(y as i32)).collect::<Vec<&i32>>().len() as i32;
-            let h_domains = horizontal_expantions.iter().filter(|rx| rx < &&(x as i32)).collect::<Vec<&i32>>().len() as i32;
-            let vert_offset: i32 = ( v_domains * DOMAIN_EXPANTION_VALUE) - v_domains;
-            let hori_offset: i32 = ( h_domains * DOMAIN_EXPANTION_VALUE) - h_domains;
+            let v_domains = vertical_expantions.iter().filter(|ry| ry < &&(y as i64)).collect::<Vec<&i64>>().len() as i64;
+            let h_domains = horizontal_expantions.iter().filter(|rx| rx < &&(x as i64)).collect::<Vec<&i64>>().len() as i64;
+            let vert_offset: i64 = ( v_domains * DOMAIN_EXPANTION_VALUE) - v_domains;
+            let hori_offset: i64 = ( h_domains * DOMAIN_EXPANTION_VALUE) - h_domains;
 
 
             if *v {
-                galaxys.push(Galaxy { x: x as i32 + hori_offset, y: y as i32 + vert_offset })
+                galaxys.push(Galaxy { x: x as i64 + hori_offset, y: y as i64 + vert_offset })
             }
         }
     }
-    let mut sum: i32 = 0;
+    let mut sum: i64 = 0;
     while !galaxys.is_empty() {
         let current = galaxys.pop().unwrap();
         for g in galaxys.iter() {
@@ -64,5 +64,5 @@ fn main() {
         
     }
 
-    println!("Task 1: {sum}")
+    println!("Task 2: {sum}");
 }
